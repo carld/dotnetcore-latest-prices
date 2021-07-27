6ab223e43f8b8ea3af3590c051ef49f87cfe20d1
Carl <105003+carld@users.noreply.github.com>
2021-07-28 01:13:45 +1200

    parameterise query so that a before date parameter can be passed in.

--- Controllers/MarketController.cs
+++ Controllers/MarketController.cs
@@ -16,7 +16,6 @@ namespace latest_prices.Controllers
     public class MarketController : ControllerBase
     {
         private readonly MarketContext db;
-
         /*
         
         The controller constructor needs to be public for dependency injection to work.
@@ -27,23 +26,37 @@ namespace latest_prices.Controllers
             this.db = dbcontext;
         }
 
-        [HttpGet("")]
+        [HttpGet]
         public List<Price> Get() => db.Prices.ToList();
 
-        [HttpGet("latest")]
-        public List<LatestPrice> Latest()
+        [Route("latest")]
+        [HttpGet]
+        public List<LatestPrice> LatestPrices(DateTime? before = null)
         {
-            return db.LatestPrices.FromSqlRaw(@"
-                SELECT p1.id, 
-                p1.ticker, 
-                p1.published_at,
-                p1.price_in_cents
-                FROM prices p1
-                JOIN
-                ( SELECT id, ticker, MAX(published_at) FROM prices GROUP BY ticker)
-                AS p2
-                ON p1.id = p2.id
-            ").ToList();
+            var parameter1 = before.HasValue ? 
+                before.Value.ToString("s") 
+                : DateTime.Now.ToString("s");
+
+            Console.WriteLine("Parameter ++ '{0}'", parameter1);
+
+            return db.LatestPrices.FromSqlRaw(
+                @"
+                    SELECT p1.id, 
+                    p1.ticker, 
+                    p1.published_at,
+                    p1.price_in_cents
+                    FROM prices p1
+                    JOIN
+                    ( SELECT id, ticker, MAX(published_at) 
+                        FROM prices 
+                        WHERE published_at < {0}
+                        GROUP BY ticker)
+                    AS p2
+                    ON p1.id = p2.id
+                    "
+                , 
+                parameter1 
+            ).ToList();
         }
     }
 }
